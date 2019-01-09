bc2ef45e255968482bdc782863663e1e4414236b
Maple Shao <939078792@qq.com>
2019-01-09 17:12:37 +0800

    add FloatDragLayout.

--- app/src/main/java/com/maple/floatdraglayout/MainActivity.java
+++ app/src/main/java/com/maple/floatdraglayout/MainActivity.java
@@ -1,52 +1,65 @@
 package com.maple.floatdraglayout;
 
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import com.maple.floatdraglayout.view.FloatDragLayout;
 
 public class MainActivity extends AppCompatActivity {
+    FloatingActionButton fab;
+    FloatDragLayout fdl_view;
+
+    Switch sw_drag;
+    Switch sw_near_screen_edge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab = findViewById(R.id.fab);
+        fdl_view = findViewById(R.id.fdl_view);
+        sw_drag = findViewById(R.id.sw_drag);
+        sw_near_screen_edge = findViewById(R.id.sw_near_screen_edge);
+
+
+        fdl_view.setPaddingRect(new RectF(10, 10, 10, 10))
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    }
+                });
+        sw_drag.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                fdl_view.setDrag(isChecked);
+            }
+        });
+        sw_near_screen_edge.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                fdl_view.setNearScreenEdge(isChecked);
+            }
+        });
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_SHORT)
                         .setAction("Action", null).show();
             }
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        // default setting
+        sw_drag.setChecked(true);
+        sw_near_screen_edge.setChecked(false);
     }
 }
